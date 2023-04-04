1110dc769167ea5774812714b7aed42875386bcc
hiroakip <hiroakip@gmail.com>
2023-04-04 00:01:09 -0400

    Update get-custom-categories.js

--- javascripts/discourse/helpers/get-custom-categories.js
+++ javascripts/discourse/helpers/get-custom-categories.js
@@ -3,13 +3,6 @@ import { withPluginApi } from "discourse/lib/plugin-api";
 import Category from "discourse/models/category";
 
 registerUnbound("getAbbreviation", categoryName => {
-  let abbr = categoryName.replace(" and", "").split(" ");
-
-  if (abbr.length > 1) {
-    abbr = abbr[0].charAt(0).toUpperCase() + abbr[1].charAt(0).toLowerCase();
-  } else {
-    abbr = abbr[0].charAt(0).toUpperCase() + abbr[0].charAt(1).toLowerCase();
-  }
-
+  let abbr = "";
   return abbr;
 });
